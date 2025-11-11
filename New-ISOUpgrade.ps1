6d1ea0d34c933ccae74fa7e220ddd38f258e2f02
saeraphas <saeraphas@users.noreply.github.com>
2025-11-11 11:13:36 -0500

    Update New-ISOUpgrade.ps1
    set iso download to blob storage URI

--- New-ISOUpgrade.ps1
+++ New-ISOUpgrade.ps1
@@ -2,7 +2,7 @@
 $provider = "Nexigen"
 $activity = "Repair"
 #URIPrompt = Read-Host "Enter the download link for the remote file source"
-$URI25H2 = "https://software-static.download.prss.microsoft.com/dbazure/888969d5-f34g-4e03-ac9d-1f9786c66749/26200.6584.250915-1905.25h2_ge_release_svc_refresh_CLIENT_CONSUMER_x64FRE_en-us.iso"
+$URI25H2 = "https://nocinstallerstorage.blob.core.windows.net/root/Windows/10.0.26200.6584/26200.6584.250915-1905.25h2_ge_release_svc_refresh_CLIENT_CONSUMER_x64FRE_en-us.iso"
 $URI = $URI25H2
 #ExpectedHash = "Enter the SHA256 hash of the remote file source"
 $ExpectedHash = "D141F6030FED50F75E2B03E1EB2E53646C4B21E5386047CB860AF5223F102A32"
@@ -86,8 +86,7 @@ if ($computedHash -ne $ExpectedHash.ToUpper()) {
         )
     
         Write-Output "Starting Windows Upgrade from ISO."
-        Write-Verbose "Setup Path: $process"
-        #Start-Process -FilePath $process -ArgumentList $argumentList -NoNewWindow -Wait
+        Start-Process -FilePath $process -ArgumentList $argumentList -NoNewWindow -Wait
 
     }
     
