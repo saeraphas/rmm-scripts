0550146d74ac86b01dc5389aa2d78fb7b43b53ba
saeraphas <saeraphas@users.noreply.github.com>
2025-11-11 10:25:02 -0500

    Update New-ISOUpgrade.ps1
    add script output and checks for file paths before using them

--- New-ISOUpgrade.ps1
+++ New-ISOUpgrade.ps1
@@ -9,28 +9,28 @@ $ExpectedHash = "D141F6030FED50F75E2B03E1EB2E53646C4B21E5386047CB860AF5223F102A3
 
 # Don't configure below here
 
-#todo, maybe: 
-#dynamically select URI and SHA256 from a table of ISOs in blob storage if there's a match on UBR? 
-# $OSBuild = "{0}.{1}.{2}.{3}" -f ('CurrentMajorVersionNumber','CurrentMinorVersionNumber','CurrentBuild','UBR' | ForEach-Object {Get-ItemPropertyValue -Path 'Registry::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion' -Name $_})
-
-
 # Check for Fortinet EDR since it breaks everything useful
+Write-Output "Checking for Fortinet EDR."
 $FortinetEDRPresent = Get-Service -Name "FortiEDR Collector Service" -ErrorAction SilentlyContinue
-if ($FortinetEDRPresent) { Write-Warning "Fortinet EDR service present on this machine. Request SOC set EDR bypass on $($Env:COMPUTERNAME)."}
+if ($FortinetEDRPresent) { Write-Warning "Fortinet EDR service present on this machine. Request SOC set EDR bypass on $($Env:COMPUTERNAME)." }
 
 $filename = [System.IO.Path]::GetFileName($URI)
 $destination = "$Env:SystemDrive\$provider\$activity\$filename"
 $folderPath = Split-Path $destination
 
 # Create destination directory and subdirectories
+Write-Output "Creating destination directory if it doesn't exist."
 $null = New-Item -Path $folderPath -ItemType Directory -Force
 
 # Check if the file already exists
+Write-Output "Checking for existing file at $destination."
 $downloadExists = Test-Path -Path $destination
 
 # Download the file only if it doesn't exist
-if (-not $downloadExists) {
-    Write-Host "Downloading file..."
+if ($downloadExists) {
+    Write-Output "File already exists, skipping download."
+} else {
+    Write-Output "File download starting."
 
     # Suppress progress bar during download
     $originalProgressPreference = $ProgressPreference
@@ -42,36 +42,53 @@ if (-not $downloadExists) {
     # Restore progress bar
     $ProgressPreference = $originalProgressPreference
 
-} else {
-    Write-Host "File already exists at $destination, skipping download."
+    Write-Output "File download completed."
 }
 
 # Verify SHA-256 hash
+Write-Output "Verifying SHA-256 hash."
 $computedHash = (Get-FileHash -Path $destination -Algorithm SHA256).Hash.ToUpper()
 if ($computedHash -ne $ExpectedHash.ToUpper()) {
     Write-Error "SHA-256 hash mismatch! Expected: $ExpectedHash, Got: $computedHash"
+    Write-Output "Delete the file or update the expected hash and try again."
     exit 1
 } else {
-    Write-Host "SHA-256 hash verified successfully."
-    Mount-DiskImage -ImagePath $destination	
-    $mountResult = Mount-DiskImage -ImagePath $destination -PassThru
-    $driveLetter = $($mountResult | Get-Volume).DriveLetter
-        
+    Write-Output "SHA-256 hash verified successfully."
+
+    Write-Output "Mounting ISO."
+    $mountedAleady = Get-DiskImage -ImagePath $destination -ErrorAction SilentlyContinue | Where-Object { $_.Attached -eq $true }
+    if ($mountedAleady) {
+        $driveLetter = $($mountedAleady | Get-Volume).DriveLetter
+        Write-Output "ISO is already mounted at $driveletter, skipping mount."
+    } else {
+        $mountResult = Mount-DiskImage -ImagePath $destination -PassThru
+        $driveLetter = $($mountResult | Get-Volume).DriveLetter
+        Write-Output "ISO mounted at drive letter: $driveLetter"
+    }
+    
     $setup = "$driveLetter" + ":\setup.exe"
+    Write-Output "Checking for setup.exe."
+    $setupExists = Test-Path -Path $setup
+    if (-not $setupExists) {
+        Write-Error "Setup.exe not found, cannot proceed with upgrade."
+        exit 1
+    } else {
+        Write-Output "Setup.exe found. Starting upgrade process."
+        $process = $setup
+        $argumentList = @(
+            "/Auto Upgrade",
+            "/DynamicUpdate Disable",
+            "/Telemetry Disable",
+            "/ShowOOBE None",
+            "/Compat IgnoreWarning",
+            "/BitLocker AlwaysSuspend",
+            "/EULA Accept"
+        )
     
-    $process = $setup
-    $argumentList = @(
-        "/Auto Upgrade",
-        "/DynamicUpdate Disable",
-        "/Telemetry Disable",
-        "/ShowOOBE None",
-        "/Compat IgnoreWarning",
-        "/BitLocker AlwaysSuspend",
-        "/EULA Accept"
-    )
-    
-Write-Output "`nStarting Windows Upgrade from ISO...`n"
-    #Start-Process -FilePath $process -ArgumentList $argumentList -NoNewWindow -Wait
+        Write-Output "Starting Windows Upgrade from ISO."
+        Write-Verbose "Setup Path: $process"
+        #Start-Process -FilePath $process -ArgumentList $argumentList -NoNewWindow -Wait
 
+    }
+    
 }
-
